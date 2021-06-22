6596dc9d021b87a0c4bca7da2b093c2bf15ab794
Pauline Trinh <paulinetrinh@Paulines-MBP.hsd1.wa.comcast.net>
2021-06-22 14:49:52 -0700

    add in automated testing

---
+++ tests/testthat/test-f_to_c.R
@@ -0,0 +1,8 @@
+test_that("f_to_c works mathematically", {
+  expect_equal(f_to_c(32), 0)
+  expect_equal(f_to_c(212), 100)
+})
+
+test_that("non-numeric input throws error", {
+  expect_error(f_to_c("a string"))
+})
